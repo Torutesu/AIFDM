f4d9d0d271161797547d8e75cf5e26f7c7585b5d
Kinjal Goswami <kinjalgoswami682@gmail.com>
2026-09-09 13:10:22 +0530

    Fix migration drift: add decision columns migration

---
+++ prisma/migrations/20260909000000_add_decisions/migration.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "Opportunity" ADD COLUMN "decidedAt" TIMESTAMP(3);
+ALTER TABLE "Opportunity" ADD COLUMN "decidedBy" TEXT;
+ALTER TABLE "Opportunity" ADD COLUMN "decisionNote" TEXT;
+ALTER TABLE "Opportunity" ADD COLUMN "dismissReason" TEXT;
